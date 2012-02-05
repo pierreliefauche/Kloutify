88342a938ce892c912ac051286ba523520cd3da8
Pierre-Élie <pierre.elie.fauche@gmail.com>
2012-02-05 13:27:27 +0100

    Firefox : mise à jour jQuery et affichage du score par défaut au lieu de ??

--- extensions/kloutify.firefoxextension/resources/jid0-w9onfk8bhsgzbaii6ilex0un4fe-at-jetpack-kloutify-lib/main.js
+++ extensions/kloutify.firefoxextension/resources/jid0-w9onfk8bhsgzbaii6ilex0un4fe-at-jetpack-kloutify-lib/main.js
@@ -6,7 +6,7 @@ exports.main = function() {
     pageMod.PageMod({
       include: "*",
       contentScriptWhen: 'ready',
-      contentScriptFile: [data.url('jquery-1.7.min.js'), data.url('script.js')],
+      contentScriptFile: [data.url('jquery-1.7.1.min.js'), data.url('script.js')],
       contentScript: 'var headID = document.getElementsByTagName("head")[0];var cssNode = document.createElement("link");cssNode.type = "text/css";cssNode.rel = "stylesheet";cssNode.href = "'+data.url('style.css')+'";headID.appendChild(cssNode);'
     });
 };
